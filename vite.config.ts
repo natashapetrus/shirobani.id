19df223a0ecdc174586bafed6c3a65f5e6924bd2
natashapetrus <github@natashapetrus.com>
2022-11-16 18:51:42 +0700

    output dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: path.resolve(__dirname, 'docs') // for Production build and GitHub Pages deployment
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
